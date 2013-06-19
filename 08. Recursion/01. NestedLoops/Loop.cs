d2dea050f0409a138b5dd1be5757c8ed332a882e
Yumer <y.yumer@gmail.com>
2013-06-19 22:19:37 +0300

    08. Recursion - SOLVED: 01, 02., 03.

---
+++ 08. Recursion/01. NestedLoops/Loop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Loop
+{
+    static void Simulate(int[] vector, int lenght, int index)
+    {
+        if (index == lenght)
+        {
+            Print(index, vector);
+            return;
+        }
+
+        for (int j = 0; j < lenght; j++)
+        {
+            vector[index] = j;
+            Simulate(vector, lenght, index + 1);
+        }
+    }
+
+    private static void Print(int length, int[] vector)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("{0} ", vector[i] + 1);
+        }
+
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        const int lenght = 4;
+        int[] taken = new int[lenght];
+
+        Simulate(taken, lenght, 0);
+    }
+}
---
+++ 08. Recursion/02-03. Combinations/Combinations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Combinations
+{
+    // Zadacha 2. i zadacha 3. 
+    static void Main()
+    {
+        int n = 2;
+        int startNum = 1;
+        int endNum = 4;
+        int[] vector = new int[n];
+
+        // GenerateCombinationsWithDuplicates(vector, 0, startNum, endNum); // zadacha 2.
+
+        GenerateCombinationsWithouthDuplicates(vector, 0, startNum, endNum); // zadacha 3.
+    }
+
+    static void GenerateCombinationsWithDuplicates(int[] arr, int index, int startNum, int endNum)
+    {
+        if (index >= arr.Length)
+        {
+            Print(arr);
+            return;
+        }
+
+        for (int i = startNum; i <= endNum; i++)
+        {
+            arr[index] = i;
+            GenerateCombinationsWithDuplicates(arr, index + 1, i, endNum);
+        }
+
+    }
+
+    static void GenerateCombinationsWithouthDuplicates(int[] arr, int index, int startNum, int endNum)
+    {
+        if (index >= arr.Length)
+        {
+            Print(arr);
+            return;
+        }
+
+        for (int i = startNum; i <= endNum; i++)
+        {
+            arr[index] = i;
+            GenerateCombinationsWithouthDuplicates(arr, index + 1, i + 1, endNum);
+
+        }
+
+    }
+
+    static void Print(int[] array)
+    {
+        Console.WriteLine("( " + string.Join(", ", array) + " )");
+    }
+}
