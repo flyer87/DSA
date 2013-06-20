a719aa22b1d7984cdbf021d5f0332f7596793131
Yumer Yumer <y.yumer@gmail.com>
2013-06-20 16:13:33 +0300

    08. Recursion - SOLVED: 06, 07., 08.

---
+++ 08. Recursion/06. GenerateSubsets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strArray = new string[] { "test", "rock", "fun" };
+        string[] result = new string[2];
+        GenerateSubsets(result, 0, 0, strArray);
+    }
+
+    private static void GenerateSubsets(string[] result, int index, int startIndex, string[] strArray)
+    {
+        if (index == result.Length)
+        {
+            Print(result);
+            return;
+        }
+
+        for (int i = startIndex; i < strArray.Length; i++)
+        {
+            result[index] = strArray[i];
+            GenerateSubsets(result, index + 1, i + 1, strArray);
+        }
+    }
+
+    private static void Print(string[] strArray)
+    {
+        Console.WriteLine("{" + string.Join(",", strArray) + "}");
+    }
+}
---
+++ 08. Recursion/07. 08. PathsInLabirinth/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    private static char[,] labyrinth = 
+    {
+        {' ', ' ', ' ', '*', ' ', ' ', ' '},
+        {'*', '*', ' ', '*', ' ', '*', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', '*', '*', '*', '*', '*', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    };
+
+    private static Stack<KeyValuePair<int, int>> path = new Stack<KeyValuePair<int, int>>();
+    private static bool pathIsFound = false;
+
+    static void Main(string[] args)
+    {
+        // Task 8.
+        // SetExit(0, 4);
+        HasPathBetweenTwoCells(0, 0);
+
+        // Task.7 
+        //SetExit(0, 4);
+        // AllPathsBetweenTwoCells(0, 0);
+
+        if (!pathIsFound)
+        {
+            Console.WriteLine("There is no path!");
+        }
+    }
+
+    private static void AllPathsBetweenTwoCells(int row, int col)
+    {
+        if (!InRange(row, col))
+        {
+            return;
+        }
+
+        if (labyrinth[row, col] == 'e')
+        {
+            PrintPath();
+            return;
+        }
+
+        if (labyrinth[row, col] != ' ')
+        {
+            return;
+        }
+
+        labyrinth[row, col] = 'v';
+        path.Push(new KeyValuePair<int, int>(row, col));
+
+        AllPathsBetweenTwoCells(row, col + 1);
+        AllPathsBetweenTwoCells(row - 1, col);
+        AllPathsBetweenTwoCells(row, col - 1);
+        AllPathsBetweenTwoCells(row + 1, col);
+
+        labyrinth[row, col] = ' ';
+        path.Pop();
+    }
+
+    private static void HasPathBetweenTwoCells(int row, int col)
+    {
+        if (pathIsFound)
+        {
+            return;
+        }
+
+        if (!InRange(row, col))
+        {
+            return;
+        }
+
+        if (labyrinth[row, col] == 'e')
+        {
+            PrintPath();
+            pathIsFound = true;
+            return;
+        }
+
+        if (labyrinth[row, col] != ' ')
+        {
+            return;
+        }
+
+        labyrinth[row, col] = 'v';
+        path.Push(new KeyValuePair<int, int>(row, col));
+
+        HasPathBetweenTwoCells(row, col + 1);
+        HasPathBetweenTwoCells(row - 1, col);
+        HasPathBetweenTwoCells(row, col - 1);
+        HasPathBetweenTwoCells(row + 1, col);
+
+        labyrinth[row, col] = ' ';
+        path.Pop();
+    }
+
+    static bool InRange(int row, int col)
+    {
+        bool rowInRange = row >= 0 && row < labyrinth.GetLength(0);
+        bool colInRange = col >= 0 && col < labyrinth.GetLength(1);
+        return rowInRange && colInRange;
+    }
+
+    private static void PrintPath()
+    {
+        KeyValuePair<int, int>[] pathArray = path.ToArray();
+        Array.Reverse(pathArray);
+        Console.WriteLine(string.Join(",", pathArray));
+    }
+
+    private static void SetExit(int row, int col)
+    {
+        labyrinth[row, col] = 'e';
+    }
+}
